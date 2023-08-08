a707afbb6145fa14aa878c88caef51677de96d34
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-08 14:00:24 +0200

    Toggling Audio
    Mapping the Actions with the State player.js

--- src/stores/player.js
+++ src/stores/player.js
@@ -18,6 +18,32 @@ export default defineStore('player', {
       })
 
       this.sound.play()
+    },
+    async toggleAudio() {
+      //this will check if the function exists on the object
+      //if not, it means that the Howl haven't being created
+      if (!this.sound.playing) {
+        return
+      }
+
+      //this will check if the audio is playing
+      if (this.sound.playing()) {
+        //if something is being played, it will be paused
+        this.sound.pause()
+      } else {
+        //if not, it will be played
+        this.sound.play()
+      }
+    }
+  },
+  getters: {
+    playing: (state) => {
+      //is the function defined?
+      if (state.sound.playing) {
+        return state.sound.playing()
+      }
+
+      return false
     }
   }
 })
