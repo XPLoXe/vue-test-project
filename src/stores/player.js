33561f94cfc86277f0b8be4b8750e7473f4684ac
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-08 13:35:52 +0200

    Playing Audio w/ Howler
    By adding Howler to the player.js the page will successfully play audio

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,12 +1,23 @@
 import { defineStore } from 'pinia'
+//the Howl object will be used to create a new audio object. we'll use it to load the audio file and control it
+import { Howl } from 'howler'
+//howler will be accessible throughout the application thanks to being in this state
 
 export default defineStore('player', {
   state: () => ({
-    current_song: {}
+    current_song: {},
+    sound: {}
   }),
   actions: {
     async newSong(song) {
       this.current_song = song
+
+      this.sound = new Howl({
+        src: [song.url],
+        html5: true //this is neccesary for the correct execution
+      })
+
+      this.sound.play()
     }
   }
 })
