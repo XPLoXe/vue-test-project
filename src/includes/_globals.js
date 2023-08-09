035d7942f5c8bcd622fbc725b51f0744ee461f69
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-09 14:57:00 +0200

    Auto-Registrering Global Components
    Button component registered globally as an example

---
+++ src/includes/_globals.js
@@ -0,0 +1,42 @@
+import _ from 'lodash'
+//https://lodash.com/
+
+export default {
+  install(app) {
+    //this is available in all JS files.
+    //it's an object of information related to the current module
+    //vite extends this function by adding a function called glob()
+    //glob imports files, it will lazy load a module
+    const baseComponents = import.meta.glob('../components/base/*.vue', {
+      //this will force the function to load the modules immediately
+      eager: true
+    })
+
+    //this function will convert each property of an objecti into an array.
+    //where the first item in the array constains the key and the second item contains the value
+    Object.entries(baseComponents).forEach(([path, module]) => {
+      //the first item in the array will have the file path,
+      //whereas the module item will store the component data
+
+      //the upperFirst() will capitalize the first character in the string
+      //the camelCase() will camel case a string.
+      //since we need it in pascal case, we need both.
+      //the split() will break the string into an array
+      //the pop() will grab the last item
+      const componentName = _.upperFirst(
+        _.camelCase(
+          path
+            .split('/')
+            .pop()
+            .replace(/\.\w+$/, '')
+        )
+      )
+      //componentName will remove the entire path and leave us just with the component
+      //example: path: ../components/base/Button.vue  -- componentName: Button
+
+      //the 'Base' is just a prefix to use as a good practice
+      //export default
+      app.component(`Base${componentName}`, module.default)
+    })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import { auth } from './includes/firebase'
 import Icon from './directives/icon'
 import i18n from './includes/i18n'
 import { registerSW } from 'virtual:pwa-register'
+import GlobalComponents from './includes/_globals'
 
 import './assets/base.css'
 import './assets/main.css'
@@ -30,6 +31,7 @@ auth.onAuthStateChanged(() => {
     app.use(router)
     app.use(VeeValidatePlugin)
     app.use(i18n)
+    app.use(GlobalComponents)
     //register directive
     app.directive('icon', Icon)
 
