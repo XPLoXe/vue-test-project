99978aab9f50595e3ef8c55d655893f0b667f343
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-09 10:12:53 +0200

    Adding Modifiers to the Directive

--- src/directives/icon.js
+++ src/directives/icon.js
@@ -2,10 +2,19 @@
 export default {
   beforeMount(el, binding) {
     //this directive will be able to output any icon
-    let iconClass = `fa fa-${binding.value} float-right text-green-400 text-xl`
+    let iconClass = `fa fa-${binding.value} text-green-400 text-xl`
 
     if (binding.arg === 'full') {
       iconClass = binding.value
+    }
+    if (binding.modifiers.right) {
+      //we have to add an space before the string
+      iconClass += ' float-right'
+    }
+    if (binding.modifiers.yellow) {
+      iconClass += ' text-yellow-400'
+    } else {
+      iconClass += ' text-green-400'
     }
 
     el.innerHTML += `<i class="${iconClass}"></i>`
