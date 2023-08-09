5b7c7c2cb8d5a1d6d9020a3f18506f5e2833ac91
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-09 12:30:55 +0200

    Importing PWA to main.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cau9rr2nqf8"
+    "revision": "0.lls17daaaho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
