ced61d6b1fec091d13ab50074b7aa8178241c950
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-09 10:05:34 +0200

    Passing Values to Directives

--- src/directives/icon.js
+++ src/directives/icon.js
@@ -1,6 +1,13 @@
 //exporting a directive
 export default {
-  beforeMount(el) {
-    el.innerHTML += '<i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>'
+  beforeMount(el, binding) {
+    //this directive will be able to output any icon
+    let iconClass = `fa fa-${binding.value} float-right text-green-400 text-xl`
+
+    if (binding.arg === 'full') {
+      iconClass = binding.value
+    }
+
+    el.innerHTML += `<i class="${iconClass}"></i>`
   }
 }
