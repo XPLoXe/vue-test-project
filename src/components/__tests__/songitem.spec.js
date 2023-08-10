f5f976877ee35d49106f93bd3500d6a4b50113d3
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-10 12:52:36 +0200

    Testing SongItem.vue

---
+++ src/components/__tests__/songitem.spec.js
@@ -0,0 +1,30 @@
+import SongItem from '@/components/SongItem.vue'
+//the RouterLinkStub component is a fake component that mimics the behavior of the router link component
+//without relying on the router library
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
+import { expect } from 'vitest'
+//import { test } from 'vitest'
+
+describe('SongItem.vue', () => {
+  test('render song.display_name', () => {
+    const song = {
+      display_name: 'test'
+    }
+
+    const wrapper = shallowMount(SongItem, {
+      props: {
+        song
+      },
+      global: {
+        components: {
+          'router-link': RouterLinkStub
+        }
+      }
+    })
+
+    const compositionAuthor = wrapper.find('.text-gray-500')
+
+    //expect(wrapper.text()).toContain(song.display_name)
+    expect(compositionAuthor.text()).toBe(song.display_name)
+  })
+})
