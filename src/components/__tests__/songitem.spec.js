803d161167ce5f9c6da69684c1df332c8a94c70f
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-10 13:43:29 +0200

    Testing Attributes of SongItem.vue
    using mock

--- src/components/__tests__/songitem.spec.js
+++ src/components/__tests__/songitem.spec.js
@@ -27,4 +27,30 @@ describe('SongItem.vue', () => {
     //expect(wrapper.text()).toContain(song.display_name)
     expect(compositionAuthor.text()).toBe(song.display_name)
   })
+
+  test('render song.docIF in id attribute', () => {
+    const song = {
+      docID: 'abc'
+    }
+
+    const wrapper = shallowMount(SongItem, {
+      props: {
+        song
+      },
+      global: {
+        components: {
+          'router-link': RouterLinkStub
+        }
+      }
+    })
+
+    const compositionAuthor = wrapper.find('.text-gray-500')
+
+    //the attributes function returns an object of attributes in the component.
+    //we can access any of the attributes in the object by their name
+    //expect(wrapper.attributes().id).toBe(`song-id-${song.docID}`)
+
+    //the wrapper.classes() will return an array of classes
+    expect(wrapper.classes()).toContain(`song-id-${song.docID}`)
+  })
 })
